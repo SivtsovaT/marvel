a94dc12d2bf3d1dfafc88f535acf078c24bcca9f
user <sivtsovatanya1985@gmail.com>
2022-05-11 14:46:36 +0300

    - realised a charInfo component;

--- src/components/charList/charList.js
+++ src/components/charList/charList.js
@@ -46,6 +46,7 @@ class CharList extends Component {
             return (
                 <li className="char__item"
                     key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}
                 >
                     <img src={item.thumbnail} alt="item.name"
                          style={imgStyle}
